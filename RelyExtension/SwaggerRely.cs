6f3b51dc607fcda24eb9ba19adfba8355cb140ce
nsb <1609806038@qq.com>
2020-05-06 22:48:29 +0800

    Swagger

---
+++ RelyExtension/SwaggerRely.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace Core.API.RelyExtension
+{
+    public static class SwaggerRely
+    {
+        /// <summary>
+        /// Api接口版本 自定义
+        /// </summary>
+        public enum ApiVersions
+        {
+
+            /// <summary>
+            /// V1 版本
+            /// </summary>
+            V1 = 1,
+            /// <summary>
+            /// V2 版本
+            /// </summary>
+            V2 = 2,
+        }
+        public static void AddSwagger(this IServiceCollection servers)
+        {
+            //nameof用于获取变量、类型或成员的简单（非限定）字符串名称。可以在错误消息中使用类型或成员的非限定字符串名称，而无需对字符串进行硬编码，这样也方便重构。
+            if (servers == null) throw new ArgumentNullException(nameof(servers));
+            var basePath = AppContext.BaseDirectory;//获取项目地址
+            servers.AddSwaggerGen(s =>
+            {
+                typeof(ApiVersions).GetEnumNames().ToList().ForEach(version =>
+                {
+                    //SwaggerDoc 定义一个或多个由Swagger生成器创建的文档
+                    //OpenApiInfo 开放API信息对象，它提供关于开放API的元数据。
+                    s.SwaggerDoc(version, new OpenApiInfo
+                    {
+                        Version = version,
+                        Title = "学习接口文档"
+                    });
+                    //OrderActionsBy 在将操作转换为操作之前，为它们提供排序的自定义策略
+                    //RelativePath 为此获取或设置相对url路径模板(相对于应用程序根目录)
+                    s.OrderActionsBy(o => o.RelativePath);
+                });
+                //Path.Combine 合并地址
+                var xmlPath = Path.Combine(basePath, "Core.API.XML");//创建的项目xml地址和名字
+                //IncludeXmlComments 为基于操作、参数和模式的操作注入对人友好的描述
+                s.IncludeXmlComments(xmlPath, true);                
+                s.OperationFilter<AddResponseHeadersFilter>();//开启加权锁
+                s.OperationFilter<AppendAuthorizeToSummaryOperationFilter>();                
+                s.OperationFilter<SecurityRequirementsOperationFilter>();// 在header中添加token，传递到后台
+            });
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using Core.API.RelyExtension;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using static Core.API.RelyExtension.SwaggerRely;
 
 namespace Core.API
 {
@@ -22,10 +25,12 @@ namespace Core.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container.此方法由运行时调用。使用此方法将服务添加到容器中。
+        //IServiceCollection 指定服务描述符集合的契约。
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwagger();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,6 +41,18 @@ namespace Core.API
                 app.UseDeveloperExceptionPage();
             }
 
+            #region Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(us =>
+            {
+                typeof(ApiVersions).GetEnumNames().OrderByDescending(ob => ob).ToList().ForEach(version => {
+                    //SwaggerEndpoint 添加Swagger JSON端点。可以是完全限定的还是相对于UI页面的
+                    us.SwaggerEndpoint($"/swagger/{version}/swagger.json", $"{version}");
+                });
+                us.RoutePrefix = "";
+                us.IndexStream = () => GetType().GetTypeInfo().Assembly.GetManifestResourceStream("Core.API.index.html");
+            });
+            #endregion
             app.UseHttpsRedirection();
 
             app.UseRouting();
